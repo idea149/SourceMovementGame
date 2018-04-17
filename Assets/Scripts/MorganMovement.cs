42e12e25809ec975367969c8413a95bf78136664
Julian (Sockman) <julianheuser@outlook.com>
2018-04-17 17:44:50 -0400

    Some fixes to some stuff and a wall-slide detector

--- Assets/Scripts/MorganMovement.cs
+++ Assets/Scripts/MorganMovement.cs
@@ -17,6 +17,7 @@ public class MorganMovement : MonoBehaviour {
 	Rigidbody rb;
 	CapsuleCollider Collider;
 	bool grounded = false;
+	int wallRun = 0; //0 is no wallrun, 1 is wallrun on left, 2 is wallrun on right
 
 	Vector3 localVelocity;
 	// Use this for initialization
@@ -36,22 +37,20 @@ public class MorganMovement : MonoBehaviour {
 		speed = Mathf.Abs(transform.InverseTransformDirection (rb.velocity).x) + Mathf.Abs(transform.InverseTransformDirection (rb.velocity).z);
 		speedText.GetComponent<Text> ().text = "Speed: " + transform.InverseTransformDirection (rb.velocity).z;
 
-
+		Debug.Log (wallRun);
 		//Detect if I'm on the ground or not
 		//Don't use the commented stuff it's a dumb capsule collider that doesn't work well
 		/*RaycastHit[] hits = Physics.CapsuleCastAll (transform.position + Collider.center+ (Vector3.one * (Collider.height / 2 - Collider.radius)), transform.position + Collider.center - (Vector3.one * (Collider.height / 2 - Collider.radius)), Collider.radius-.1f, Vector3.down, .4f);
 		foreach (RaycastHit objectHit in hits) {
 			grounded = (objectHit.transform.tag == "WorldGeometry");
 		}*/
-
+		//detect if grounded
 		if (Physics.Raycast (transform.position, Vector3.down, (Collider.height / 2) + Collider.radius)) {
 			grounded = true;
 			rb.velocity = new Vector3 (rb.velocity.x, 0, rb.velocity.z);
 		} else {
 			grounded = false;
 		}
-
-
 		//movement
 		if (grounded) {
 			moveSpeed = groundSpeed;
@@ -59,23 +58,48 @@ public class MorganMovement : MonoBehaviour {
 		else {
 			moveSpeed = airSpeed;
 		}
-		if (Input.GetKey ("w")) {
-			//transform.Translate (Vector3.forward * moveSpeed * realtime);
-			rb.AddForce (transform.forward * moveSpeed);
-		}
-		if (Input.GetKey ("a")) {
-			//transform.Translate (Vector3.left * moveSpeed * realtime);
-			rb.AddForce (-(transform.right * moveSpeed));
-		}
-		if (Input.GetKey ("s")) {
-			//transform.Translate (Vector3.back * moveSpeed * realtime);
-			rb.AddForce (-(transform.forward * moveSpeed));
-		}
-		if (Input.GetKey ("d")) {
-			//transform.Translate (Vector3.right * moveSpeed * realtime);
-			rb.AddForce (transform.right * moveSpeed);
-		}
+		if (grounded) {
+			if (Input.GetKey ("w")) {
+				//transform.Translate (Vector3.forward * moveSpeed * realtime);
+				rb.AddForce (transform.forward * moveSpeed);
+			}
+			if (Input.GetKey ("a")) {
+				//transform.Translate (Vector3.left * moveSpeed * realtime);
+				rb.AddForce (-(transform.right * moveSpeed));
+			}
+			if (Input.GetKey ("s")) {
+				//transform.Translate (Vector3.back * moveSpeed * realtime);
+				rb.AddForce (-(transform.forward * moveSpeed));
+			}
+			if (Input.GetKey ("d")) {
+				//transform.Translate (Vector3.right * moveSpeed * realtime);
+				rb.AddForce (transform.right * moveSpeed);
+			}
 
+		}
+		else {
+			//air strafe
+			if (rb.rotation.eulerAngles.y > prevAngle && Input.GetKey ("d")) {
+				rb.AddForce (transform.forward * (groundSpeed));
+			}
+			if (rb.rotation.eulerAngles.y < prevAngle && Input.GetKey ("a")) {
+				rb.AddForce (transform.forward * (groundSpeed));
+			}
+
+			//detect wallrunning
+			if (Physics.Raycast (transform.position, Vector3.left, Collider.radius + .5f)) {
+				wallRun = 1;
+				rb.velocity = new Vector3 (rb.velocity.x, 0, rb.velocity.z);
+			} 
+			else if (Physics.Raycast (transform.position, Vector3.right, Collider.radius + 0.5f)) {
+				wallRun = 2;
+				rb.velocity = new Vector3 (rb.velocity.x, 0, rb.velocity.z);
+			} 
+			else {
+				wallRun = 0;
+			}
+		}
+		prevAngle = rb.rotation.eulerAngles.y;
 		//top grounded speed fix
 		if (grounded) {
 			rb.velocity = new Vector3(Mathf.Round (rb.velocity.x * 100)/100, Mathf.Round (rb.velocity.y * 100)/100, Mathf.Round (rb.velocity.z * 100)/100);
@@ -97,20 +121,5 @@ public class MorganMovement : MonoBehaviour {
 		if (Input.GetKey ("space") && grounded) {
 			rb.AddForce(0, jumpHeight, 0);
 		}
-
-
-
-
-
-		//air strafe
-		if (rb.rotation.eulerAngles.y > prevAngle && !grounded && Input.GetKey ("d")) {
-			rb.AddForce(transform.forward * (groundSpeed * 2));
-		}
-		if (rb.rotation.eulerAngles.y < prevAngle && !grounded && Input.GetKey ("a")) {
-			rb.AddForce(transform.forward * (groundSpeed * 2));
-		}
-
-		prevAngle = rb.rotation.eulerAngles.y;
-
 	}
 }
