785b963fe9e9290c5b128b3ba91ba0e602d4530a
Julian (Sockman) <julianheuser@outlook.com>
2018-12-24 14:37:09 -0500

    i think the movement is okay now

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class playerMovement : MonoBehaviour {
 
 	public GameObject speedText;
@@ -13,7 +14,8 @@ public class playerMovement : MonoBehaviour {
 	public float airaccel;
 
 	bool grounded;
-	bool bhopfix;
+    bool bhopCheck;
+    bool bhopfix;
 	Rigidbody rb;
 	CapsuleCollider Collider;
 	Vector3 moveGoal;
@@ -21,6 +23,9 @@ public class playerMovement : MonoBehaviour {
 
     float playerSpeed = 0; //Read-only speed var
 	float lastGroundSpeed = 0;	//Read-only: how fast the player was on the ground only
+
+    bool boxCastHit;
+    RaycastHit hit;
 
 	// Use this for initialization
 	void Start () {
@@ -34,17 +39,21 @@ public class playerMovement : MonoBehaviour {
         playerSpeed = rb.velocity.magnitude;//Mathf.Abs(rb.velocity.x) + Mathf.Abs(rb.velocity.z); //calculate speed
 
         speedText.GetComponent<Text>().text = "Speed: " + Mathf.Round(playerSpeed); //display speed
-		moveGoal = getMoveGoal();	//Get the direction I want to go in
+		moveGoal = getMoveGoal();   //Get the direction I want to go in
 
-		//detect if on ground
-		// Old Method: if (Physics.Raycast (transform.position, -gameObject.transform.up, (Collider.height / 2) + 0.01f )) {
-		if (Physics.CapsuleCast (new Vector3(transform.position.x,transform.position.y + (Collider.height/4),transform.position.z), new Vector3(transform.position.x,transform.position.y-(Collider.height/4),transform.position.z), Collider.radius - .1f, Vector3.down, .11f)) {
+        //detect if on ground
+        // Old Method: if (Physics.Raycast (transform.position, -gameObject.transform.up, (Collider.height / 2) + 0.01f )) {
+        //Physics.CapsuleCast(new Vector3(transform.position.x, transform.position.y + (Collider.height / 4), transform.position.z), new Vector3(transform.position.x, transform.position.y - (Collider.height / 4), transform.position.z), Collider.radius - .1f, Vector3.down, .11f);
+        //boxCastHit = Physics.BoxCast(transform.position + new Vector3(0,1,0), new Vector3(Collider.radius - 0.1f, Collider.height / 2, Collider.radius - 0.1f), Vector3.down, out hit, transform.rotation, (Collider.height/2) + 0.01f);
+
+        boxCastHit = Physics.SphereCast(transform.position, Collider.radius, Vector3.down, out hit, (Collider.height / 4) + 0.01f);
+        if (boxCastHit && hit.normal != Vector3.right && hit.normal != Vector3.left && hit.normal != Vector3.forward && hit.normal != Vector3.back) {
 			grounded = true;
 		} else {
 			grounded = false;
-		}
+        }
 
-		if (grounded && bhopfix) {	//I'm on the ground
+		if (grounded && bhopCheck) {	//I'm on the ground
 			lastGroundSpeed = Mathf.Abs(rb.velocity.x) + Mathf.Abs(rb.velocity.z); //Get speed on ground
 			if (moveGoal != Vector3.zero) {	//Do I want to move?
 				if (playerSpeed <= maxSpeed) {
@@ -59,37 +68,21 @@ public class playerMovement : MonoBehaviour {
 			}
 		}
 		else{ //Whee I'm flying
-
-            Vector3 velCut = new Vector3(rb.velocity.x,0,rb.velocity.z);
-
-			float currentspeed =  Vector3.Dot(velCut, moveGoalMod);
-			float currentspeedNormalized =  Vector3.Dot(velCut.normalized, moveGoalMod);
-            Debug.Log(currentspeedNormalized);
-
-            if (Mathf.Abs(currentspeedNormalized) < 1.1 && Mathf.Abs(currentspeedNormalized) > .9) {
-                //rb.velocity = rb.velocity;
-			}
-			else{
-				float addspeed = ((Mathf.Abs (moveGoalMod.x) + Mathf.Abs (moveGoalMod.z))) - currentspeed;
-				if (moveGoalMod != Vector3.zero) {
-					rb.velocity += (moveGoalMod / ((Mathf.Abs (moveGoalMod.x) + Mathf.Abs (moveGoalMod.z)))) * addspeed;
-				}
-			}
-
-
-			//Test for Air movement, Enable if you want broken air movement
-			//Vector3 moveGoalNormal = Vector3.Normalize(moveGoal);
-			//rb.velocity = new Vector3(moveGoalNormal.x * lastGroundSpeed,rb.velocity.y, moveGoalNormal.z * lastGroundSpeed);
-		}
+            bhopfix = true;
+            airMovement();
+        }
 
 		//Jump
-		if (Input.GetKey ("space")) {
-			if (grounded == true){
-				rb.AddForce (transform.up * jumpHeight);
-			}
-			bhopfix = false;
+		if (Input.GetKey ("space")) { 
+			if (grounded && bhopfix)
+            {
+                Debug.Log("JUMP");
+                rb.AddForce (transform.up * jumpHeight);
+                bhopfix = false;
+            }
+            bhopCheck = false;
 		} else {
-			bhopfix = true;
+            bhopCheck = true;
 		}
 
 
@@ -98,7 +91,7 @@ public class playerMovement : MonoBehaviour {
     Vector3 getMoveGoal()
     {
 
-        Vector3 endDirection = new Vector3(0, 0, 0);
+        Vector3 endDirection = Vector3.zero;
         if (Input.GetKey("w"))
         {
             endDirection += transform.forward;
@@ -121,5 +114,61 @@ public class playerMovement : MonoBehaviour {
 
         endDirection.Normalize();
         return (endDirection);
+    }
+
+    void airMovement()
+    {
+        if(moveGoal == Vector3.zero)
+        {
+            return; //if the player doesn't want to move, don't move
+        }
+
+        Vector3 velCut = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+
+        float currentspeed = Vector3.Dot(velCut, moveGoalMod);
+        float currentspeedNormalized = Vector3.Dot(velCut.normalized, moveGoalMod);
+
+        float rawCurrentspeedNormalized = Vector3.Dot(velCut.normalized, moveGoal);
+
+
+        if (Mathf.Abs(currentspeedNormalized) < 1.1 && Mathf.Abs(currentspeedNormalized) > .9)
+        {
+            //rb.velocity = rb.velocity;
+        }
+        else
+        {
+            float addspeed = ((Mathf.Abs(moveGoalMod.x) + Mathf.Abs(moveGoalMod.z))) - currentspeed;
+            if (moveGoalMod != Vector3.zero)
+            {
+                rb.velocity += (moveGoalMod / ((Mathf.Abs(moveGoalMod.x) + Mathf.Abs(moveGoalMod.z)))) * addspeed;
+            }
+        }
+
+        if (Mathf.Abs(rawCurrentspeedNormalized) < .8 && Mathf.Abs(rawCurrentspeedNormalized) > .6)
+        {
+            rb.velocity += transform.forward * .1f;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(transform.position, Collider.radius);
+
+        Gizmos.color = Color.red;
+        //Check if there has been a hit yet
+        if (boxCastHit)
+        {
+            Gizmos.DrawWireSphere(hit.point, Collider.radius);
+        }
+        //If there hasn't been a hit yet, draw the ray at the maximum distance
+        else
+        {
+            //Draw a Ray forward from GameObject toward the maximum distance
+            Gizmos.DrawRay(transform.position, Vector3.down * ((Collider.height / 4) + 0.01f));
+            //Draw a cube at the maximum distance
+            Gizmos.DrawWireSphere(transform.position + Vector3.down * ((Collider.height / 4) + 0.01f), Collider.radius);
+        }
     }
 }
